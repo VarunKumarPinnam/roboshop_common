73bef5e9e4c2e562e3a123fe6ae7631472383a78
Varun <varunkumar.pinnam@gmail.com>
2026-02-03 20:40:43 +0530

    updated varaiables templates

--- frontend.sh
+++ frontend.sh
@@ -21,24 +21,24 @@ systemctl enable nginx &>>$LOGS_FILE
 validation $? "$G enabled nginx service $N"
 
 systemctl start nginx
-validation $? "$G started nginx service $N"
+validation $? "$Gstarted nginx service $N"
 
 
 rm -rf /usr/share/nginx/html/* 
-validation $? "$Y removed default html content $N"
+validation $? "$Yremoved default html content $N"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
-validation $? "$Y fronted code downloaded $N"
+validation $? "$Yfronted code downloaded $N"
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOGS_FILE
-validation $? "$Y frontend code unzipped to html folder $N"
+validation $? "$Yfrontend code unzipped to html folder $N"
 
 sed -i d /etc/nginx/nginx.conf 
-validation $? "$Y removed default nginx config data $N"
+validation $? "$Yremoved default nginx config data $N"
 
 cp $SHELL_DIR/nginx.conf /etc/nginx/nginx.conf
-validation $? "$Y updated new configuration to nginx.conf file $N"
+validation $? "$Yupdated new configuration to nginx.conf file $N"
 
 systemctl restart nginx 
-validation $? "$Y Nginx service restarted $N"
+validation $? "$YNginx service restarted $N"
